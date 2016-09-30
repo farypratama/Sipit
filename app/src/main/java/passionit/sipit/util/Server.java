5e2cc58ce12df44d444d29f20334bff55e711e92
Muhamad Fary Pratama <farypratama@gmail.com>
2016-09-30 11:41:55 +0700

    Initial commit

---
+++ app/src/main/java/passionit/sipit/util/Server.java
@@ -0,0 +1,9 @@
+package passionit.sipit.util;
+
+/**
+ * Created by Fary & Rafa on 18/06/2016.
+ */
+public class Server {
+    // sesuaikan dengan IP Address PC/laptop atau ip address emulator android 10.0.2.2
+    public static final String URL = "http://192.168.43.184/json2/";
+}
